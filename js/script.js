99548a01fdbb5e4235afc92e1b85330c9de7a135
Vivek Nair <vnair611@gmail.com>
2020-09-05 18:31:54 -0700

    Make the starter files

--- js/script.js
+++ js/script.js
@@ -1,59 +1,48 @@
-const ENTER_KEY_CODE = 13
 
-const taskInput = document.getElementById('task')
-const taskList = document.getElementById('task-list')
-
-taskInput.addEventListener("keydown", event => {
-  if (event.keyCode === ENTER_KEY_CODE) {
-    event.preventDefault()
-    addTaskToList()
-  }
-})
-
-/* Adds a new task item to the list, with the value from the
- * given input.
- *
- * Arguments:
- * taskInput -- the HTMLElement input tag
- */
-function addTaskToList() {
-  if (taskInput.value)  {
-
-    // Create list parent
-    const li = document.createElement('li')
-    li.textContent = taskInput.value
-
-    // Create delete button
-    const deleteBn = document.createElement('span')
-    deleteBn.classList.add("delete")
-
-    li.appendChild(deleteBn)
-
-    // Create check button
-    const checkBn = document.createElement('span')
-    checkBn.classList.add("check")
-
-    li.appendChild(checkBn)
-
-    addTaskListeners(checkBn, deleteBn)
-    taskInput.value = ""
-    taskList.appendChild(li)
+class TasksApp extends React.Component {
+  render() {
+    return (
+      <>
+        <TaskItems />
+        <TaskCreator addTask={this.handleAddTask} />
+      </>
+    )
   }
 }
 
-/* Handles check/delete events for the given task.
- *
- * Arguments:
- * taskLi -- the HTMLElement li tag
- */
-function addTaskListeners(checkBn, deleteBn) {
-  checkBn.addEventListener('click', () => {
-    const li = checkBn.parentNode
-    li.classList.toggle('done')
-  })
-
-  deleteBn.addEventListener('click', () => {
-    const li = deleteBn.parentNode
-    li.parentNode.removeChild(li)
-  })
+class TaskCreator extends React.Component {
+  render() {
+    return (
+      <div id="task-container">
+        <textarea id="task" name="task" placeholder="I need to...">
+        </textarea>
+      </div>
+    )
+  }
 }
+class TaskItems extends React.Component {
+  render() {
+    return (
+      <div id="sidebar">
+        <h1>Task List</h1>
+        <p>Add tasks to your list by typing to the right and press-ing enter. You may then view pending tasks below.</p>
+
+        <ul id="task-list">
+        </ul>
+      </div>
+    )
+  }
+}
+
+class TaskListItem extends React.Component {
+  render() {
+    return (
+      <li></li>
+    )
+  }
+}
+
+ReactDOM.render(
+  <TasksApp />,
+  document.getElementById('wrapper')
+)
